3523565433fe112b48bb78f7a1863735e79e29cf
briskt <3172830+briskt@users.noreply.github.com>
2024-04-18 14:43:30 -0600

    test folder

---
+++ test/main.tf
@@ -0,0 +1,43 @@
+module "minimal" {
+  source = "../"
+
+  email_from_address = "no_reply@example.com"
+}
+
+module "all" {
+  source = "../"
+
+  email_from_address  = "no_reply@example.com"
+  create_smtp_user    = true
+  smtp_user_name      = "smtp-user-example-com"
+  create_ecs_role     = true
+  ecs_role_name       = "ecs-role-example-com"
+  create_spf_record   = true
+  spf_record_text     = "v=spf1 include:amazonses.com -all"
+  create_dmarc_record = true
+  dmarc_record_text   = "v=DMARC1; p=none; sp=reject"
+  mail_from_subdomain = "bounce"
+  extra_tags          = { customer = "Acme, Inc." }
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+locals {
+  aws_region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+    cloudflare = {
+      version = "~> 4.0"
+      source  = "cloudflare/cloudflare"
+    }
+  }
+}
