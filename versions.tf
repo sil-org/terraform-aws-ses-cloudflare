0756aa29c13040be8c5bdb7416fcdf42c7edbc5d
briskt <3172830+briskt@users.noreply.github.com>
2024-04-18 14:34:20 -0600

    correct incompatible provider version constraints

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      version = "~> 5.0"
+      version = ">=4.0.0, <6.0"
       source  = "hashicorp/aws"
     }
     cloudflare = {
-      version = "~> 4.0"
+      version = ">=3.0.0, <5.0"
       source  = "cloudflare/cloudflare"
     }
   }
