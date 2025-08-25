7477675b38d2e07006fae0724f52eb4d80243055
Matt H. <matt_henderson@sil.org>
2025-08-25 14:50:34 -0400

    Remove extra comment, add other new variable to "all" test

--- test/main.tf
+++ test/main.tf
@@ -18,9 +18,8 @@ module "all" {
   dmarc_record_text   = "v=DMARC1; p=none; sp=reject"
   mail_from_subdomain = "bounce"
   cloudflare_tags     = ["customer:Acme, Inc."]
-
-  # new optional feature
   create_bounce_topic = true
+  bounce_topic_name   = "example-ses-bounces"
 }
 
 provider "aws" {
