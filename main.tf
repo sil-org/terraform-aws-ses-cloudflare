e8874e5222e0f330d07759ad4a0442f82e99de1a
Matt H. <matt_henderson@sil.org>
2025-08-25 15:33:01 -0400

    Adjust how SES identity is passed to notification topic (per docs)

--- main.tf
+++ main.tf
@@ -230,7 +230,7 @@ resource "aws_sns_topic_policy" "ses_publish" {
 resource "aws_ses_identity_notification_topic" "bounce" {
   count = var.create_bounce_topic ? 1 : 0
 
-  identity          = one(aws_ses_domain_identity.this[*].domain)
+  identity          = one(aws_ses_domain_identity.this[*].arn)
   notification_type = "Bounce"
   topic_arn         = one(aws_sns_topic.ses_bounces[*].arn)
 }
