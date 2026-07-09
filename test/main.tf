b18c594ff557ef75d528424bebcb0d48825530cb
Praveen <praveen_koilraj@sil.org>
2026-07-09 21:02:02 +0530

    module parameters in main.tf

--- test/main.tf
+++ test/main.tf
@@ -7,20 +7,18 @@ module "minimal" {
 module "all" {
   source = "../"
 
-  email_from_address        = "no_reply@example.com"
-  create_smtp_user          = true
-  smtp_user_name            = "smtp-user-example-com"
-  create_ecs_role           = true
-  ecs_role_name             = "ecs-role-example-com"
-  create_spf_record         = true
-  spf_record_text           = "v=spf1 include:amazonses.com -all"
-  mail_from_spf_record_text = "\"v=spf1 mx include:amazonses.com -all\""
-  create_dmarc_record       = true
-  dmarc_record_text         = "v=DMARC1; p=none; sp=reject"
-  mail_from_subdomain       = "bounce"
-  cloudflare_tags           = ["customer:Acme, Inc."]
-  create_bounce_topic       = true
-  bounce_topic_name         = "example-ses-bounces"
+  email_from_address  = "no_reply@example.com"
+  create_smtp_user    = true
+  smtp_user_name      = "smtp-user-example-com"
+  create_ecs_role     = true
+  ecs_role_name       = "ecs-role-example-com"
+  create_spf_record   = true
+  create_dmarc_record = true
+  dmarc_record_text   = "v=DMARC1; p=none; sp=reject"
+  mail_from_subdomain = "bounce"
+  cloudflare_tags     = ["customer:Acme, Inc."]
+  create_bounce_topic = true
+  bounce_topic_name   = "example-ses-bounces"
 }
 
 provider "aws" {
